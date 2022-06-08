29329f9ac1ad2cf3421f55a076abb4648ad3d887
SITENDA <74384378+SITENDA@users.noreply.github.com>
2022-06-09 06:22:47 +0800

    Moved my old files from the first build icon to the cloned GitHub project

---
+++ public/script.js
@@ -0,0 +1,12 @@
+
+
+  const swup = new Swup()
+
+  function fives()
+  { 
+      setTimeout(myURL, 5000);
+  }
+
+         function myURL(){
+            window.open('http://127.0.0.1:5500/views/login.html');
+         }
